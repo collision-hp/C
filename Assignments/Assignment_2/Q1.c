37ed478c0bc00d655134f33e664ac3b69e9b20a6
ChampatiSatyabrata <141308691+collision-hp@users.noreply.github.com>
2024-11-11 17:58:04 +0530

    Add files via upload

--- Assignments/Assignment_2/Q1.c
+++ Assignments/Assignment_2/Q1.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
-
 int main(){
-    
-    return 0;
+int la=345;
+float fb=4.5;
+char var='Z';
+printf("%d %p\n",la,&la);
+printf("%f %p\n",fb,&fb);
+printf("%c %p\n",var,&var);
+return 0;
 }
