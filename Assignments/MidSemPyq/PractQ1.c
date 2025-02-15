9c761095d13d68600045b9c0fb2810d8bbb10eaa
ChampatiSatyabrata <141308691+collision-hp@users.noreply.github.com>
2025-02-15 18:12:30 +0530

    Add files via upload

---
+++ Assignments/MidSemPyq/PractQ1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    char str[]="missisippi"
+    for (int i = 0; i < strlen(str)/2; i++){
+        str[i]=str[strlen(str)-i-1]
+        str
+    }
+    
+    return 0;
+}
